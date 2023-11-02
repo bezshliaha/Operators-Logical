1f14654a0e7149f5008a122637b2bd31656f3705
bezshliaha <bezshliaha@gmail.com>
2023-11-02 16:43:40 +0100

    Update tax rate percent

--- src/app/Main01.java
+++ src/app/Main01.java
@@ -4,15 +4,15 @@ public class Main01 {
 
     public static void main(String[] args) {
 
-        double income = 10000;
+        double income = 8000;
         double tax;
 
         if (income <= 10000) {
-            tax = income * 0.025;
+            tax = income * 2.5 / 100;
         } else if (income <= 25000) {
-            tax = 10000 * 0.025 + (income - 10000) * 0.043;
+            tax = 10000 * 2.5 / 100 + (income - 10000) * 4.3 / 100;
         } else {
-            tax = 10000 * 0.025 + 15000 * 0.043 + (income - 25000) * 0.067;
+            tax = 10000 * 2.5 / 100 + 15000 * 4.3 / 100 + (income - 25000) * 6.7 / 100;
         }
         System.out.printf("Tax amount: %.2f%n", tax);
     }
